a7a2ed9334f8cd988dbc7d8cc9523896709b806a
HammerHand92 <david.portillo92@gmail.com>
2020-01-18 13:26:57 -0500

    updated messages

--- js/utils/messages.js
+++ js/utils/messages.js
@@ -4,12 +4,12 @@ export const fieldMessage = (adjutant, value, rule, field) => {
 	if (adjutant === 'advisable') {
 		if (field === 'price') return `${fieldSpec[field].tag} can be empty, but providing this<br/>information would help us determine the tax`;
 	} 
-	else if (adjutant === 'required') 				return `${fieldSpec[field].tag} cannot be empty`;
-	else if (adjutant === 'minLength') 				return `${fieldSpec[field].tag} must be at least ${rule} characters long.<br/>You've provided ${value.length} characters`;
-	else if (adjutant === 'maxLength') 				return `${fieldSpec[field].tag} cannot exceed ${rule} characters.<br/>You've provided ${value.length} characters`;
-	else if (adjutant === 'numbersOnly') 			return `${fieldSpec[field].tag} can only be numbers`
-	else if (adjutant === 'numbersOnly->minValue') 	return `${fieldSpec[field].tag} must be at least ${rule}`
-	else if (adjutant === 'numbersOnly->maxValue') 	return `${fieldSpec[field].tag} cannot be more than ${rule}`
+	else if (adjutant === 'required') 				return `${fieldSpec[field].tag} field cannot be empty`;
+	else if (adjutant === 'minLength') 				return `${fieldSpec[field].tag} field must be at least ${rule} characters long.<br/>You've provided ${value.length} characters`;
+	else if (adjutant === 'maxLength') 				return `${fieldSpec[field].tag} field cannot exceed ${rule} characters.<br/>You've provided ${value.length} characters`;
+	else if (adjutant === 'numbersOnly') 			return `${fieldSpec[field].tag} field can only be numbers`
+	else if (adjutant === 'numbersOnly->minValue') 	return `${fieldSpec[field].tag} field must be at least ${rule}`
+	else if (adjutant === 'numbersOnly->maxValue') 	return `${fieldSpec[field].tag} field cannot be more than ${rule}`
 	else if (adjutant === 'dateField->month') 		return `${fieldSpec[field].tag} field contains an invalid month.<br/>Format: ${rule}`
 	else if (adjutant === 'dateField->year') 		return `${fieldSpec[field].tag} field contains an invalid year.<br/>Format: ${rule}`
 	else if (adjutant === 'dateField->day') 		return `${fieldSpec[field].tag} field contains an invalid day.<br/>Format: ${rule}`
