cd921db2f8573f4c0440954e511432bf45721780
David <david.portillo92@gmail.com>
2020-02-09 00:09:11 -0500

    restructored file inspector

--- js/utils/fileInspector.js
+++ js/utils/fileInspector.js
@@ -1,3 +1,28 @@
+import { trackerIssue } from './overseer.js'
+import { showNotify } from './notification.js'
+import { sampleData, gridOptions } from '../main.js'
+
 export const inspectFileExtension = () => {
     console.log("File extension inspector")
+    console.log(trackerIssue)
+    trackerIssue.errorCount += 1;
 }
+
+window.onDragOver = (event) => {
+	sampleData[0].make = 'new value';
+	// console.log(gridOptions.api);
+	// console.log("opening notification for testing")
+	showNotify("attempting to upload a file");
+	var params = {
+		force : true
+	};
+	gridOptions.api.refreshCells(params);
+	// console.log(sampleData);
+	inspectFileExtension()
+	event.preventDefault();
+};
+
+window.onDrop = (event) => {
+	console.log(event);
+	
+};
