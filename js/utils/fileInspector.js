3a8eaef13d0457d54ea9a5fde590cb9c08d80978
David <david.portillo92@gmail.com>
2020-02-09 00:12:39 -0500

    removed console log

--- js/utils/fileInspector.js
+++ js/utils/fileInspector.js
@@ -3,26 +3,20 @@ import { showNotify } from './notification.js'
 import { sampleData, gridOptions } from '../main.js'
 
 export const inspectFileExtension = () => {
-    console.log("File extension inspector")
-    console.log(trackerIssue)
     trackerIssue.errorCount += 1;
 }
 
 window.onDragOver = (event) => {
 	sampleData[0].make = 'new value';
-	// console.log(gridOptions.api);
-	// console.log("opening notification for testing")
 	showNotify("attempting to upload a file");
 	var params = {
 		force : true
 	};
 	gridOptions.api.refreshCells(params);
-	// console.log(sampleData);
 	inspectFileExtension()
 	event.preventDefault();
 };
 
 window.onDrop = (event) => {
 	console.log(event);
-	
 };
