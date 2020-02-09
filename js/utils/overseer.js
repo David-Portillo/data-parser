a92f2216b3c46fa8c4ff548668e527afd3dc8795
HammerHand92 <david.portillo92@gmail.com>
2020-02-09 14:30:30 -0500

    re-written overseer using Object.defineProperties

--- js/utils/overseer.js
+++ js/utils/overseer.js
@@ -1,7 +1,55 @@
-// create this using prototype (check book)
-export let trackerIssue = {
-	errorCount  : 0,
-	uploadingOk : false,
-	parsingOk   : false
-};
+function Overseer() {
+	let _errorCount = 0;
+	let _fileCompliant = false;
+	let _parsingCompleted = false;
 
+	Object.defineProperty(this, 'errorCount', {
+		enumerable: true,
+		configurable: false,
+		get: function() {
+			console.log(`Retrieving errorCount value, yield ${_errorCount}`);
+			return _errorCount;
+		},
+		set: function(newValue) {
+			console.log(`Updating errorCount value to ${newValue}`)
+			_errorCount = newValue;
+		},
+	});
+
+	Object.defineProperty(this, 'fileCompliant', {
+		enumerable: true,
+		configurable: false,
+		get: function() {
+			console.log(`Retrieving fileCompliant value, yield ${_fileCompliant}`);
+			return _fileCompliant;
+		},
+		set: function(newValue) {
+			console.log(`Updating fileCompliant value to ${newValue}`)
+			_fileCompliant = newValue;
+		},
+	});
+
+	Object.defineProperty(this, 'parsingCompleted', {
+		enumerable: true,
+		configurable: false,
+		get: function() {
+			console.log(`Retrieving parsingCompleted value, yield ${_parsingCompleted}`);
+			return _parsingCompleted;
+		},
+		set: function(newValue) {
+			console.log(`Updating parsingCompleted value to ${newValue}`)
+			_parsingCompleted = newValue;
+		},
+	});
+
+	Object.defineProperty(this, 'properties', {
+		value: function() { console.table({_errorCount, _fileCompliant, _parsingCompleted}) },
+		writable: false,
+		enumerable: false,
+		configurable: false
+	})
+
+	Object.preventExtensions(this);
+}
+
+export let overseer = new Overseer();
