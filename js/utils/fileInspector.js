0a3ffdcbcefd4cc11873f2bb6f2374b15bfd63d0
David <david.portillo92@gmail.com>
2020-02-09 01:12:02 -0500

    updated code

--- js/utils/fileInspector.js
+++ js/utils/fileInspector.js
@@ -2,20 +2,32 @@ import { trackerIssue } from './overseer.js'
 import { showNotify } from './notification.js'
 import { sampleData, gridOptions } from '../main.js'
 
+const transmogrifyDropzone = ({ event = null}) => {
+	let dropzone = document.querySelector('#dropzone > div')
+	dropzone.removeAttribute('class')
+	dropzone.setAttribute('class', `dropzone-${event}`)
+}
+
 export const inspectFileExtension = () => {
     trackerIssue.errorCount += 1;
 }
 
 window.onDragOver = (event) => {
-	sampleData[0].make = 'new value';
+	transmogrifyDropzone({ event: 'ongoing'})
 	showNotify("attempting to upload a file");
-	var params = {
-		force : true
-	};
+
+	console.log(dropzone)
+	sampleData[0].make = 'new value';
+	
+	let params = { force : true };
 	gridOptions.api.refreshCells(params);
 	inspectFileExtension()
 	event.preventDefault();
 };
+
+window.onDragLeave = (event) => {
+	transmogrifyDropzone({ event: 'standby'})
+}
 
 window.onDrop = (event) => {
 	console.log(event);
