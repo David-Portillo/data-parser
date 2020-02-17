990fa1313189186f9dab3c154014cefdf84678fd
HammerHand92 <david.portillo92@gmail.com>
2020-02-17 17:10:10 -0500

    refactored code

--- js/utils/common.js
+++ js/utils/common.js
@@ -1,44 +1,36 @@
 import {overseer} from './overseer.js'
 
+// Constants section
+
 export const constants = {
   acceptableExtensions: [ 'xlsx', 'csv' ]
 }
+
+// Notification section
+
+export const showNotify = ({message = '...', event = null}) => {
+  let notify = document.querySelector('#notify > div')
+  notify.parentElement.style.display = 'flex'
+  notify.removeAttribute('class');
+  notify.setAttribute('class',`notify-${event}`)
+  notify.firstElementChild.textContent = message;
+}
+
+export const closeNotify = () => {
+  document.querySelector('#notify').style.display = 'none';
+}
+
+window.closeNotify = () => {
+  closeNotify();
+};
+
+// Dropzone section
 
 export const transmogrifyDropzone = ({event = null}) => {
 	let dropzone = document.querySelector('#dropzone > div');
 	dropzone.removeAttribute('class');
 	dropzone.setAttribute('class', `dropzone-${event}`);
 };
-
-export const showNotify = ({message = 'No message is set', event = null}) => {
-  document.querySelector('#notify > div').style.display = 'block';
-  document.querySelector('#notify > div > p').textContent = message;
-}
-
-export const displayResetButton = ({filename}) => {
-  transmogrifyDropzone({ event: 'standby' });
-	document.getElementById('dropzone').style.display = 'none';
-  document.getElementById('filename').textContent = filename
-	document.getElementById('reset').style.display = 'block';
-}
-
-export const resetAppStatus = () => {
-  overseer.reset();
-  overseer.properties();
-  document.getElementById('fileupload').value = null;
-	document.getElementById('dropzone').style.display = 'flex'
-  document.getElementById('filename').textContent = '...'
-  document.getElementById('reset').style.display = 'none';
-}
-
-window.closeNotify = () => {
-  document.querySelector('#notify > div').style.display = 'none';
-};
-
-window.onResetFile = (event) => {
-  event.preventDefault();
-  resetAppStatus();
-}
 
 window.onDragOver = (event) => {
 	event.preventDefault();
@@ -50,4 +42,34 @@ window.onDragLeave = (event) => {
 	transmogrifyDropzone({event: 'standby'});
 };
 
+// Resetting section
 
+export const inspection = {
+  reset: () => {
+    overseer.reset();
+    overseer.properties();
+    transmogrifyDropzone({ event: 'standby' });
+    closeNotify();
+    document.getElementById('fileupload').value = null;
+    document.getElementById('dropzone').style.display = 'flex'
+    document.getElementById('filename').textContent = '...'
+    document.getElementById('reset').style.display = 'none';
+  },
+  success: ({filename}) => {
+    document.getElementById('dropzone').style.display = 'none';
+    document.getElementById('filename').textContent = filename
+    document.getElementById('reset').style.display = 'block';
+  },
+  error: () => {
+    transmogrifyDropzone({ event: 'standby' });
+    document.getElementById('fileupload').value = null;
+    document.getElementById('dropzone').style.display = 'flex'
+    document.getElementById('filename').textContent = '...'
+    document.getElementById('reset').style.display = 'none';
+  }
+}
+
+window.onResetFile = (event) => {
+  event.preventDefault();
+  inspection.reset();
+}
