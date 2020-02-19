505ab897797d0fdb95bf8a65a1b92b320742e335
HammerHand92 <david.portillo92@gmail.com>
2020-02-18 22:35:22 -0500

    re-factored code. added sample logic to simulate updating grid after data sanitation. removed overseer.

--- js/utils/fileInspector.js
+++ js/utils/fileInspector.js
@@ -1,61 +1,72 @@
-import { overseer } from './overseer.js';
-import { showNotify, inspectionOutcome, constants } from './common.js'
+import { grid } from '../grid/grid.js';
+import InspectionException from '../handlers/inspectionException.js';
 import { notifyMessage as nm } from '../specs/messageSpec.js';
-import InspectionException from '../exceptions/inspectionException.js'
-import { sampleData, gridOptions } from '../main.js';
+import { constants, inspectionOutcome, showNotify } from './common.js';
 
-window.inspectFile = ({input, uploadType = 'dropzone'}) => {
+window.inspectFile = ({ input, uploadType = 'dropzone' }) => {
 	event.preventDefault();
 
 	let file = uploadType === 'browse' ? input.files[0] : input.dataTransfer.items[0].getAsFile();
 	let fileExtension = file.name.split('.').pop();
 
 	try {
-
-		if (!constants.acceptableExtensions.includes(fileExtension)) 
-			throw new InspectionException({ et: 'ie_ife', nt: { msg: nm.invalidFileExt, ev: 'error' }})
+		if (!constants.acceptableExtensions.includes(fileExtension))
+			throw new InspectionException({ et: 'ie_ife', nt: { msg: nm.invalidFileExt, ev: 'error' } });
 
 		let reader = new FileReader();
-		let fileDetails = {}
+		let fileDetails = {};
 
 		reader.readAsArrayBuffer(file);
 		reader.onload = (e) => {
-			const opts = { type: 'array', cellDates: true }
+			const opts = { type: 'array', cellDates: true, raw: true };
 			const rawData = new Uint8Array(e.target.result);
 			const workbook = XLSX.read(rawData, opts);
 			const sheetName = workbook.SheetNames[0];
 			const worksheet = workbook.Sheets[sheetName];
 			let jsonWS = XLSX.utils.sheet_to_json(worksheet, { blankRows: false });
 
-			console.log(jsonWS);
+			// console.log(workbook)
+			// console.log(worksheet)
+			// console.log(jsonWS);
 
-			fileDetails.name = `${file.name} - ${sheetName}`
+			fileDetails.name = `${file.name} - ${sheetName}`;
 
+			// simulating when data has been sanitized
+
+			jsonWS = [
+				{
+					make  : jsonWS[0].Maker,
+					model : jsonWS[0].Model,
+					price : 10,
+					date  : jsonWS[0].Date
+				},
+				{
+					make  : jsonWS[0].Maker,
+					model : jsonWS[0].Model,
+					price : '',
+					date  : jsonWS[0].Date
+				}
+			];
+
+			grid.api.updateRowData({ add: jsonWS });
 		};
 
 		reader.onloadend = (e) => {
-			if (e.target.error) 
-				throw new InspectionException({ et: 'ie_fre', nt: { msg: nm.fileError, ev: 'error' } })
-				
-			console.log('continue with logic...')
-			overseer.fileCompliant = true;
-			overseer.properties();
-			inspectionOutcome.success({filename: fileDetails.name})
-			showNotify({message: nm.inspectionPassed, event: 'success'})
+			if (e.target.error) throw new InspectionException({ et: 'ie_fre', nt: { msg: nm.fileError, ev: 'error' } });
 
+			console.log('continue with logic...');
+			inspectionOutcome.success({ filename: fileDetails.name });
+			showNotify({ message: nm.inspectionPassed, event: 'success' });
 		};
 
 		reader.onerror = (e) => {
-			console.log("an error occurred")
-			console.log(e)
-		}
-
+			console.log('an error occurred');
+			console.log(e);
+		};
 	} catch (error) {
-		overseer.fileCompliant = false;
-		overseer.properties();
-		console.log(error.toString())
-		showNotify({message: error.notify.msg, event: error.notify.ev});
-		inspectionOutcome.error()
+		console.log(error.toString());
+		showNotify({ message: error.notify.msg, event: error.notify.ev });
+		inspectionOutcome.error();
 		return 0;
 	}
 };
