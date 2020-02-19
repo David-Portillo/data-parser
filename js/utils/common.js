52e27c977ec5a7e6525be0fcb04f85fdc7adc99f
HammerHand92 <david.portillo92@gmail.com>
2020-02-18 22:33:07 -0500

    added grid to be used when resetting, added overseer to use file compliant

--- js/utils/common.js
+++ js/utils/common.js
@@ -1,32 +1,33 @@
-import {overseer} from './overseer.js'
+import { overseer } from './overseer.js';
+import { grid } from '../grid/grid.js';
 
 // Constants section
 
 export const constants = {
-  acceptableExtensions: [ 'xlsx', 'csv' ]
-}
+	acceptableExtensions : [ 'xlsx', 'csv' ]
+};
 
 // Notification section
 
-export const showNotify = ({message = '...', event = null}) => {
-  let notify = document.querySelector('#notify > div')
-  notify.parentElement.style.display = 'flex'
-  notify.removeAttribute('class');
-  notify.setAttribute('class',`notify-${event}`)
-  notify.firstElementChild.textContent = message;
-}
+export const showNotify = ({ message = '...', event = null }) => {
+	let notify = document.querySelector('#notify > div');
+	notify.parentElement.style.display = 'flex';
+	notify.removeAttribute('class');
+	notify.setAttribute('class', `notify-${event}`);
+	notify.firstElementChild.textContent = message;
+};
 
 export const closeNotify = () => {
-  document.querySelector('#notify').style.display = 'none';
-}
+	document.querySelector('#notify').style.display = 'none';
+};
 
 window.closeNotify = () => {
-  closeNotify();
+	closeNotify();
 };
 
 // Dropzone section
 
-export const transmogrifyDropzone = ({event = null}) => {
+export const transmogrifyDropzone = ({ event = null }) => {
 	let dropzone = document.querySelector('#dropzone > div');
 	dropzone.removeAttribute('class');
 	dropzone.setAttribute('class', `dropzone-${event}`);
@@ -34,42 +35,45 @@ export const transmogrifyDropzone = ({event = null}) => {
 
 window.onDragOver = (event) => {
 	event.preventDefault();
-	transmogrifyDropzone({event: 'ongoing'});
+	transmogrifyDropzone({ event: 'ongoing' });
 };
 
 window.onDragLeave = (event) => {
 	event.preventDefault();
-	transmogrifyDropzone({event: 'standby'});
+	transmogrifyDropzone({ event: 'standby' });
 };
 
 // Resetting section
 
 export const inspectionOutcome = {
-  reset: () => {
-    overseer.reset();
-    overseer.properties();
-    transmogrifyDropzone({ event: 'standby' });
-    closeNotify();
-    document.getElementById('fileupload').value = null;
-    document.getElementById('dropzone').style.display = 'flex'
-    document.getElementById('filename').textContent = '...'
-    document.getElementById('reset').style.display = 'none';
-  },
-  success: ({filename}) => {
-    document.getElementById('dropzone').style.display = 'none';
-    document.getElementById('filename').textContent = filename
-    document.getElementById('reset').style.display = 'block';
-  },
-  error: () => {
-    transmogrifyDropzone({ event: 'standby' });
-    document.getElementById('fileupload').value = null;
-    document.getElementById('dropzone').style.display = 'flex'
-    document.getElementById('filename').textContent = '...'
-    document.getElementById('reset').style.display = 'none';
-  }
-}
+	reset   : () => {
+		overseer.reset();
+		overseer.properties();
+		grid.api.setRowData([]);
+		transmogrifyDropzone({ event: 'standby' });
+		closeNotify();
+		document.getElementById('fileupload').value = null;
+		document.getElementById('dropzone').style.display = 'flex';
+		document.getElementById('filename').textContent = '...';
+		document.getElementById('reset').style.display = 'none';
+	},
+	success : ({ filename }) => {
+    overseer.fileCompliant = true;
+		document.getElementById('dropzone').style.display = 'none';
+		document.getElementById('filename').textContent = filename;
+		document.getElementById('reset').style.display = 'block';
+	},
+	error   : () => {
+    overseer.fileCompliant = false;
+		transmogrifyDropzone({ event: 'standby' });
+		document.getElementById('fileupload').value = null;
+		document.getElementById('dropzone').style.display = 'flex';
+		document.getElementById('filename').textContent = '...';
+		document.getElementById('reset').style.display = 'none';
+	}
+};
 
 window.onResetFile = (event) => {
-  event.preventDefault();
-  inspection.reset();
-}
+	event.preventDefault();
+	inspectionOutcome.reset();
+};
