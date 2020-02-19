edbe26624f9dd5705406e394a08a168c1d7f1b4d
HammerHand92 <david.portillo92@gmail.com>
2020-02-18 22:28:52 -0500

    formatted field spec

--- js/specs/fieldSpec.js
+++ js/specs/fieldSpec.js
@@ -1,24 +1,22 @@
 export const fieldSpecification = {
-  make: {
-    id: "make",
-    tag: "Maker",
-    rules: ["maxLength|6"]
-  },
-  model: {
-    id: "model",
-    tag: "Model",
-    rules: ["required|true", "minLength|7", "maxLength|10"]
-  },
-  price: {
-    id: "price",
-    tag: "Price",
-    rules: [
-      "advisable",'numbersOnly|{"minValue": "100.50", "maxValue": "100000"}'
-    ]
-  },
-  date: {
-    id: "date",
-    tag: "Date",
-    rules: ["required|false", 'dateField|{"format": "DD/MM/YYYY"}']
-  }
+	make  : {
+		id    : 'make',
+		tag   : 'Maker',
+		rules : [ 'maxLength|6' ]
+	},
+	model : {
+		id    : 'model',
+		tag   : 'Model',
+		rules : [ 'required|true', 'minLength|7', 'maxLength|10' ]
+	},
+	price : {
+		id    : 'price',
+		tag   : 'Price',
+		rules : [ 'advisable', 'numbersOnly|{"minValue": "100.50", "maxValue": "100000"}' ]
+	},
+	date  : {
+		id    : 'date',
+		tag   : 'Date',
+		rules : [ 'required|false', 'dateField|{"format": "DD/MM/YYYY"}' ]
+	}
 };
