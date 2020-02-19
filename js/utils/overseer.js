8773011a5376840107e2c1e56c78cf0c9b2fa471
HammerHand92 <david.portillo92@gmail.com>
2020-02-18 22:35:59 -0500

    removed data variable

--- js/utils/overseer.js
+++ js/utils/overseer.js
@@ -1,83 +1,69 @@
+import { grid } from '../grid/grid.js';
+
 function Overseer() {
 	let _errorCount = 0;
 	let _fileCompliant = false;
 	let _parsingCompleted = false;
-	let _data = []
 
 	Object.defineProperty(this, 'errorCount', {
-		enumerable: true,
-		configurable: false,
-		get: function() {
+		enumerable   : true,
+		configurable : false,
+		get          : function() {
 			console.log(`Retrieving errorCount value, yield ${_errorCount}`);
 			return _errorCount;
 		},
-		set: function(newValue) {
-			console.log(`Updating errorCount value to ${newValue}`)
+		set          : function(newValue) {
+			console.log(`Updating errorCount value to ${newValue}`);
 			_errorCount = newValue;
-		},
+		}
 	});
 
 	Object.defineProperty(this, 'fileCompliant', {
-		enumerable: true,
-		configurable: false,
-		get: function() {
+		enumerable   : true,
+		configurable : false,
+		get          : function() {
 			console.log(`Retrieving fileCompliant value, yield ${_fileCompliant}`);
 			return _fileCompliant;
 		},
-		set: function(newValue) {
-			console.log(`Updating fileCompliant value to ${newValue}`)
+		set          : function(newValue) {
+			console.log(`Updating fileCompliant value to ${newValue}`);
 			_fileCompliant = newValue;
-		},
+		}
 	});
 
 	Object.defineProperty(this, 'parsingCompleted', {
-		enumerable: true,
-		configurable: false,
-		get: function() {
+		enumerable   : true,
+		configurable : false,
+		get          : function() {
 			console.log(`Retrieving parsingCompleted value, yield ${_parsingCompleted}`);
 			return _parsingCompleted;
 		},
-		set: function(newValue) {
-			console.log(`Updating parsingCompleted value to ${newValue}`)
+		set          : function(newValue) {
+			console.log(`Updating parsingCompleted value to ${newValue}`);
 			_parsingCompleted = newValue;
-		},
+		}
 	});
 
-	Object.defineProperty(this, 'data', {
-		enumerable: false,
-		configurable: false,
-		get: function() {
-			console.log(`Retrieving data value, yield ${_data}`);
-			return _data;
-		},
-		set: function(newValue) {
-			console.log(`Updating data value to ${newValue}`);
-			_data = newValue;
-		}
-	})
-
 	Object.defineProperty(this, 'reset', {
-		value: function(){
+		value        : function() {
 			console.log('resetting overseer...');
 			_errorCount = 0;
-		 	_fileCompliant = false;
-		 	_parsingCompleted = false;
-		 	_data = []
+			_fileCompliant = false;
+			_parsingCompleted = false;
 		},
-		writable: false,
-		enumerable: false,
-		configurable: false
-	})
+		writable     : false,
+		enumerable   : false,
+		configurable : false
+	});
 
 	Object.defineProperty(this, 'properties', {
-		value: function() { 
-			console.table({_errorCount, _fileCompliant, _parsingCompleted })
-			console.table(_data)
+		value        : function() {
+			console.table({ _errorCount, _fileCompliant, _parsingCompleted });
 		},
-		writable: false,
-		enumerable: false,
-		configurable: false
-	})
+		writable     : false,
+		enumerable   : false,
+		configurable : false
+	});
 
 	Object.preventExtensions(this);
 }
