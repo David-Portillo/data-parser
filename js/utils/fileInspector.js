72ab063e2ef247d9a05157afd4ee5f8e41ce0ace
David <david.portillo92@gmail.com>
2020-02-24 22:17:33 -0500

    imported grid back added key normalizer method modified key remover logic modified key locator logic cleaned a little bit of messy code

--- js/utils/fileInspector.js
+++ js/utils/fileInspector.js
@@ -1,35 +1,47 @@
+import {grid} from '../grid/grid.js';
 import InspectionException from '../handlers/inspectionException.js';
-import { aliases } from '../specs/fieldSpec.js';
-import { notifyMessage as nm } from '../specs/messageSpec.js';
-import { constants, inspectionOutcome, showNotify } from './common.js';
-import { overseer } from './overseer.js';
+import {aliases, fieldKeys} from '../specs/fieldSpec.js';
+import {notifyMessage as nm} from '../specs/messageSpec.js';
+import {constants, inspectionOutcome, showNotify} from './common.js';
+import {overseer} from './overseer.js';
 
 const stringComparator = () => {};
 
-const keyRemover = ({ set = [ {} ], strip = [] }) => {
-	set.map((r) => {
-		Object.keys(r).map((k) => {
-			if (strip.includes(k)) return delete r[k];
-		});
+const keyNormalizer = ({ set = [], keyCase = 'lower' }) => {
+	let normalizedKeySet = [];
+
+	set.forEach((o) => {
+		normalizedKeySet.push(Object.keys(o).reduce((acc, k) => {
+			return {...acc, [keyCase === 'upper' ? k.toLocaleUpperCase(overseer.locale) : k.toLocaleLowerCase(overseer.locale)]: o[k]};
+		}, {}));
 	});
-	return set;
+
+	return normalizedKeySet;
 };
 
-const keyLocator = ({ efk = [], sfk = aliases() }) => {
-	console.log('system field and aliases: ', sfk);
-	console.log('external field headers: ', efk);
-	console.log('navigator languages: ', overseer.navDialect);
+const keyRemover = ({ set = [], strip = [] }) => { 
+	set.map((r) => { Object.keys(r).map((k) => { if (strip.includes(k)) return delete r[k]; }); });
+	return set
+};
 
+const keyLocator = ({efk = [], sfa = aliases()}) => {
+	console.log('system field and aliases: ', sfa);
+	console.log('external field headers: ', efk);
 	return {};
 };
 
 const interpretFile = ({ data }) => {
-	const efk = Object.keys(data[0]);
-	const outcome = keyLocator({ efk });
+	data = keyNormalizer({ set: data, keyCase: 'lower'});
+	data = keyRemover({set: data, strip: [ '__empty' ]});
+	
+	const outcome = keyLocator({}); // need to pass keys from all the objects, but no duplicates
+
 	console.log(data);
+
+	return data
 };
 
-window.depository = ({ input, uploadType = 'dropzone' }) => {
+window.depository = ({input, uploadType = 'dropzone'}) => {
 	event.preventDefault();
 
 	let file = uploadType === 'browse' ? input.files[0] : input.dataTransfer.items[0].getAsFile();
@@ -37,34 +49,35 @@ window.depository = ({ input, uploadType = 'dropzone' }) => {
 
 	try {
 		if (!constants.acceptableExtensions.includes(fileExtension))
-			throw new InspectionException({ et: 'ie_ife', nt: { msg: nm.invalidFileExt, ev: 'error' } });
+			throw new InspectionException({et: 'ie_ife', nt: {msg: nm.invalidFileExt, ev: 'error'}});
 
 		let reader = new FileReader();
 		let fileDetails = {};
 
 		reader.readAsArrayBuffer(file);
 		reader.onload = (e) => {
-			const opts = { type: 'array', cellDates: true, raw: true };
+			const opts = {type: 'array', cellDates: true, raw: true};
 			const rawData = new Uint8Array(e.target.result);
 			const workbook = XLSX.read(rawData, opts);
 			const sheetName = workbook.SheetNames[0];
 			const worksheet = workbook.Sheets[sheetName];
-			let jsonWS = XLSX.utils.sheet_to_json(worksheet);
 
 			fileDetails.name = `${file.name} - ${sheetName}`;
+			fileDetails.data = XLSX.utils.sheet_to_json(worksheet);
 
-			jsonWS = keyRemover({ set: jsonWS, strip: [ '__EMPTY' ] });
-			interpretFile({ data: jsonWS });
-
-			//grid.api.updateRowData({ add: jsonWS });
 		};
 
 		reader.onloadend = (e) => {
-			if (e.target.error) throw new InspectionException({ et: 'ie_fre', nt: { msg: nm.fileError, ev: 'error' } });
+			if (e.target.error) throw new InspectionException({et: 'ie_fre', nt: {msg: nm.fileError, ev: 'error'}});
 
-			console.log('continue with logic...');
-			inspectionOutcome.success({ filename: fileDetails.name });
-			showNotify({ message: nm.inspectionPassed, event: 'success' });
+			console.log('loading file finished, continue with logic...');
+
+			interpretFile({ data: fileDetails.data });
+
+			// grid.api.updateRowData({add: [ {} ] });
+			inspectionOutcome.success({filename: fileDetails.name});
+
+			showNotify({message: nm.inspectionPassed, event: 'success'});
 		};
 
 		reader.onerror = (e) => {
@@ -73,7 +86,7 @@ window.depository = ({ input, uploadType = 'dropzone' }) => {
 		};
 	} catch (error) {
 		console.log(error.toString());
-		showNotify({ message: error.notify.msg, event: error.notify.ev });
+		showNotify({message: error.notify.msg, event: error.notify.ev});
 		inspectionOutcome.error();
 		return 0;
 	}
